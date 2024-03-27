b5624a05b0d970404569a2edb4832dd27a99c3c8
NoopyDx <38747915+NoopyDx@users.noreply.github.com>
2024-03-27 15:49:29 +0100

    Update luidz.js

--- luidz.js
+++ luidz.js
@@ -1,5 +1,10 @@
 console.log(window.location);
-function getParams(a) {
+const hasVisited = Cookies.get("has-luidz") == undefined;
+if (hasVisited) {
+  console.log("jamais")
+  console.log(Cookies.get("has-luidz"))
+  Cookies.set("has-luidz", "true");
+  function getParams(a) {
   var b = document.getElementsByTagName("script");
   for (var i = 0; i < b.length; i++) {
     if (b[i].src.indexOf("/" + a) > -1) {
@@ -13,41 +18,46 @@ function getParams(a) {
     }
   }
   return {};
+  }
+  
+  const modal = document.createElement("div");
+  document.body.appendChild(modal);
+  modal.style.width = "512px";
+  modal.style.height = "512px";
+  modal.style.position = "fixed";
+  modal.style.top = "50%";
+  modal.style.left = "50%";
+  modal.style.transform = "translate(-50%, -50%)";
+  modal.style.backgroundColor = "red";
+  modal.style.borderRadius = "8px";
+  modal.style.zIndex = "1000";
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "Close";
+  modal.appendChild(closeButton);
+  closeButton.addEventListener("click", () => {
+    modal.remove();
+  });
+  const iframe = document.createElement("iframe");
+  iframe.width = "100%";
+  iframe.height = "100%";
+  iframe.src = "https://luids.vercel.app/?id=" + getParams("luidz.js")["id"];
+  iframe.style.position = "absolute";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.style.zIndex = "-1";
+  iframe.style.borderRadius = "8px";
+  modal.appendChild(iframe);
+  window.addEventListener(
+    "message",
+    function (event) {
+      if (event.data === "fermerModal") {
+        modal.remove();
+      }
+    },
+    false
+  );
+} else {
+  console.log("déjà")
+  console.log(Cookies.get("has-luidz"))
+  //It's not a new user
 }
-
-const modal = document.createElement("div");
-document.body.appendChild(modal);
-modal.style.width = "512px";
-modal.style.height = "512px";
-modal.style.position = "fixed";
-modal.style.top = "50%";
-modal.style.left = "50%";
-modal.style.transform = "translate(-50%, -50%)";
-modal.style.backgroundColor = "red";
-modal.style.borderRadius = "8px";
-modal.style.zIndex = "1000";
-const closeButton = document.createElement("button");
-closeButton.textContent = "Close";
-modal.appendChild(closeButton);
-closeButton.addEventListener("click", () => {
-  modal.remove();
-});
-const iframe = document.createElement("iframe");
-iframe.width = "100%";
-iframe.height = "100%";
-iframe.src = "https://luids.vercel.app/?id=" + getParams("luidz.js")["id"];
-iframe.style.position = "absolute";
-iframe.style.top = "0";
-iframe.style.left = "0";
-iframe.style.zIndex = "-1";
-iframe.style.borderRadius = "8px";
-modal.appendChild(iframe);
-window.addEventListener(
-  "message",
-  function (event) {
-    if (event.data === "fermerModal") {
-      modal.remove();
-    }
-  },
-  false
-);
